236e67cda946c33e54ea5dc43f2798b783644d95
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2024-10-30 21:23:09 +0300

    v.1.10. Added logo and fixed bugs

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -5,6 +5,7 @@ import Navigation from "../Navigation/Navigation";
 import UserMenu from "../UserMenu/UserMenu";
 import AuthNav from "../AuthNav/AuthNav";
 import styles from "./AppBar.module.css";
+import logo from "../../assets/images/logo.png";
 
 export default function AppBar() {
   const user = useSelector(selectUser);
@@ -15,8 +16,9 @@ export default function AppBar() {
       {isLoggedIn ? (
         <p className={styles.text}>Welcome, {user.name}</p>
       ) : (
-        <Link className={styles.text} to="/">
-          Phonebook
+        <Link className={styles.headerLogo} to="/">
+          <img src={logo} className={styles.logo} alt="Phonebook Logo" />
+          <p className={styles.phonebookText}>Phonebook</p>
         </Link>
       )}
       <div className={styles.navigationItems}>
